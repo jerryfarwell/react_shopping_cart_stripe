ab47a75401222f2db0c4b2fa293e9a813a3637a5
JERRY farwell <114482666+jerryfarwell@users.noreply.github.com>
2023-06-03 12:04:00 +0200

    trying bootstrap

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,21 +8,12 @@ import { Col, Row } from 'react-bootstrap'
 function Testimonials() {
   return (
     <div>
-        <Row >
-         <Col className="col-md-4">
-           <img src={testimonial_first} alt="" />
-         </Col>
-         <Col>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
-         
-         </Col>
-         <Col>
+        <Row>
+  <Col className="col-md-4">
+    <img src={testimonial_first} alt="" style={{ maxWidth: '100%' }} />
+  </Col>         
+  
+  <Col className="col-md-8">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat sit labore, odit id, nostrum animi ducimus libero asperiores, atque quasi illum eos omnis repudiandae vero voluptatum ipsa est pariatur.</p>
